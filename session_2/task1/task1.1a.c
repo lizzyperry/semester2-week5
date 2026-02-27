201afbf2029f2da60edb1bd298e23939018e8e99
lizzyperry <fzjm0636@leeds.ac.uk>
2026-02-27 13:01:14 +0000

    uysfuys

--- session_2/task1/task1.1a.c
+++ session_2/task1/task1.1a.c
@@ -7,9 +7,14 @@ int main(void){
  * Complete the following for loop in C to print all numbers divisible by 7 below 100.
  */
      // complete your code here
-     for(int i=1; ; ){
-		 // todo
-	 }
-
+    int i,n=7,a=100;
+    printf("Numbers divisible by 7 are:\n");
+    for(i=1;i<=a;i++)
+    {
+        if(i%7==0)
+        {
+        printf("%d\n",i);
+        }
+    }
     return 0;
 }
